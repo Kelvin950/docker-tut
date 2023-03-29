27f87a1572a862ab264f64c3ba251edad0bee6f7
kelvin <denlinato@gmail.com>
2023-03-29 09:12:30 +0000

    connecting containers

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("hello world!!");
 });
 app.listen(8080, () => {
   console.log("server opened");
